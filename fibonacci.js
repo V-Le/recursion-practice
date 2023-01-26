4331bbda632b0d363cb72438760d5a381626d4c9
Victor WSG-Laptop <le.victor@hotmail.com>
2023-01-26 16:19:19 -0600

    fibonacci practice

--- fibonacci.js
+++ fibonacci.js
@@ -7,5 +7,24 @@ Using iteration, write a function fibs which takes a number and returns an array
 Now write another method fibsRec which solves the same problem recursively. This can be done in just a couple of lines (or 1 if you’re crazy, but don’t consider either of these lengths a requirement… just get it done). */
 
 function fibs(num) {
-  
+  let fibArray = [];
+  let firstNum = 0;
+  let secNum = 1;
+  let newNum = 0;
+
+  if (num <= 1) {
+    return 0;
+  }
+
+  for (let i=0; i <= num-1; i++) {
+    fibArray.push(newNum);
+    newNum = firstNum + secNum;
+    firstNum = secNum;
+    secNum = newNum;
+  }
+  return fibArray;
 }
+
+console.log(fibs(8))
+
+
