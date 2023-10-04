9c244add6960665096e73117dd89992147647b8d
Xian-T580 <80217731+V-Le@users.noreply.github.com>
2023-10-03 21:34:22 -0500

    recursion refresher - check array

--- recursionTwo.js
+++ recursionTwo.js
@@ -23,3 +23,14 @@ function factorial(n) {
 }
 console.log(factorial(3));
 
+// Question 4: Check all values in an array
+// Write a function called all which accepts an array and a callback and returns true if every value in the array returns true when passed as parameter to the callback function
+var numArr = [1,2,9];
+var numArrTwo = [2,4,6];
+function lets(Arr) {
+  if (Arr.length == 0) return "true";
+  if (Arr.pop() > 7) { 
+    return "false";
+  } else return lets(Arr);
+}
+console.log(lets(numArrTwo))
