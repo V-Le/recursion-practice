490f558a21a8eab2194328b7eadf0b5279a8fd02
Xian-T580 <80217731+V-Le@users.noreply.github.com>
2023-10-03 21:30:52 -0500

    recursion refresher - power function

--- recursionTwo.js
+++ recursionTwo.js
@@ -1,9 +1,16 @@
 //https://www.codingame.com/playgrounds/5422/js-interview-prep-recursion
 // Question 1: Sum all numbers
 // Write a function called sumRange. It will take a number and return the sum of all numbers from 1 up to the number passed in.
-
 function sumRange(n) {
   if (n == 1) return 1;
   return n + sumRange(n-1)
 }
 console.log(sumRange(4));
+
+// Question 2: Power function
+// Write a function called power which takes in a base and an exponent. If the exponent is 0, return 1.
+function power(n, e) {
+  if (e == 0) return 1;
+  return n * power(n, e-1);
+}
+console.log(power(2, 4));
