32cafceb62bba7cb9143882f0bc0ac3e1dadb954
Xian-T580 <80217731+V-Le@users.noreply.github.com>
2023-10-10 21:54:30 -0500

    recursion practice - sum of squares

--- recursionTwo.js
+++ recursionTwo.js
@@ -108,3 +108,32 @@
 //     }
 //     return total + totalIntegers(arr);
 // }
+
+// // Question 8:
+// // Write a function that sums squares of numbers in list that may contain more lists
+// console.clear();
+
+// var l = [[1], 2, 3];
+// console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+// l = [[1, 2], 3];
+// console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+// l = [[[[[[[[[1]]]]]]]]];
+// console.log(SumSquares(l)); // 1 = 1
+
+// l = [10, [[10], 10], [10]];
+// console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
+// function SumSquares(arr) {
+//     if (arr.length == 0) return 0;
+//     let total = 0;
+//     let first = arr.shift();
+
+//     if (Array.isArray(first)) {
+//         total += SumSquares(first);
+//     } else if (Number.isInteger(first)) {
+//         let squareInt = first ** 2;
+//         total += squareInt;
+//     }
+//     return total + SumSquares(arr);
+// }
